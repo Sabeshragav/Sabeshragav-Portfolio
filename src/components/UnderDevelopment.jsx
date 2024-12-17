432496d27fcdc6d0f1e38fe6ce1351c9b7b2c868
Sabeshragav <sabeshragav289@gmail.com>
2024-12-17 21:21:33 +0530

    under development

--- src/components/UnderDevelopment.jsx
+++ src/components/UnderDevelopment.jsx
@@ -5,7 +5,7 @@ export default function UnderDevelopment() {
     <div className="fixed inset-0 h-screen flex items-center justify-center bg-gray-900 bg-opacity-70 backdrop-blur-xl z-50">
       <div className="bg-gray-900 border border-gray-600 rounded-lg shadow-lg p-8 m-4">
         <p className="text-white text-center text-xl">
-          Site is under development
+          Site is under development !
         </p>
         <div className="flex justify-end w-full mt-1">
           <p>- Sabeshragav</p>
