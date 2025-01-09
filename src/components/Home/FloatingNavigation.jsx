70408397136a32ea2c4352cb661f9963ef94f0bf
Sabeshragav <sabeshragav289@gmail.com>
2025-01-09 12:55:44 +0530

    Removed Intro

--- src/components/Home/FloatingNavigation.jsx
+++ src/components/Home/FloatingNavigation.jsx
@@ -45,7 +45,7 @@ export default function FloatingNavigation() {
           <div className="flex-center bg-slate-700 w-fit rounded-full px-4 sm:px-6 py-3">
             <ul className="flex space-x-2 sm:space-x-3">
               <NavList elementId={"welcome"} label={"Top"} />
-              <NavList elementId={"intro"} label={"Intro"} />
+              {/* <NavList elementId={"intro"} label={"Intro"} /> */}
               <NavList elementId={"featured_projects"} label={"Projects"} />
               <NavList elementId={"about"} label={"About"} />
               <NavList elementId={"skills"} label={"Skills"} />
