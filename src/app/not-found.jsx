20e5fb88142f3d07b0472b146e58a78d11fc5741
Sabeshragav <sabeshragav289@gmail.com>
2025-01-17 23:31:03 +0530

    Altered Not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen black_gray_gradient text-gray-100">
-      <h1 className="text-5xl font-bold mb-4 text-center">
+      <h1 className="text-5xl font-bold mb-4 text-center text-red-600">
         404 - Page Not Found
       </h1>
       <p className="text-lg mb-6 text-center">
