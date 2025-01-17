965dcfa3591194b73ccbb4ed10cc93c907a1d897
Sabeshragav <sabeshragav289@gmail.com>
2025-01-17 11:17:59 +0530

    Updated Skills Section

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import Loader from "@/components/Loader";
 
 export default function ProjectLoading() {
   return (
-    <div className="h-[700px] flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <Loader />
     </div>
   );
