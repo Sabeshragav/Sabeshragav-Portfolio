a993c9cd129c2863e017e9dedeb2afd9c63263e1
Sabeshragav <sabeshragav289@gmail.com>
2025-01-17 11:39:38 +0530

    Added login in Services

--- src/app/(with-navbar)/page.jsx
+++ src/app/(with-navbar)/page.jsx
@@ -4,7 +4,7 @@ const logo = "/icons/logo.svg";
 
 export const metadata = {
   icons: logo,
-  title: "Sabeshragav",
+  title: "Sabeshragav's Portfolio",
   description:
     "A professional platform showcasing innovative projects, insightful blogs, and opportunities to connect with Sabeshragav G K, a dedicated expert in technology and creativity.",
 };
