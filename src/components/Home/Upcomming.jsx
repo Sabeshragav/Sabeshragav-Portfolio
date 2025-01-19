99086af7e2f2e77dcb8491325cdc5a04b216b046
Sabeshragav <sabeshragav289@gmail.com>
2025-01-19 17:05:58 +0530

    Updated Upcomming Projects

--- src/components/Home/Upcomming.jsx
+++ src/components/Home/Upcomming.jsx
@@ -16,7 +16,7 @@ export default function Upcomming({ ParallaxText }) {
   ];
 
   return (
-    <section className="min-h-screen py-44 px-4 md:px-8">
+    <section className="py-44 px-4 md:px-8">
       <h2 className="text-4xl md:text-5xl font-bold mb-12 text-center">
         <ParallaxText className={"mb-16 leading-snug"}>
           Upcomming {list?.length > 1 ? "Projects" : "Project"}
