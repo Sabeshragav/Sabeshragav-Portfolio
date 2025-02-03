3dda98fc7a7355aa5fa114e6222472ff32411ba7
Sabeshragav <sabeshragav289@gmail.com>
2025-02-03 08:29:07 +0530

    updated upcomming projects

--- src/components/Home/Upcomming.jsx
+++ src/components/Home/Upcomming.jsx
@@ -9,6 +9,12 @@ export default function Upcomming({ ParallaxText }) {
         "This task involves developing a social-driven ecommerce platform that integrates social networking features with online shopping, enabling users to share, discuss, and shop for products seamlessly within the same ecosystem.",
       message: "Stay Tuned",
     },
+    {
+      title: "Spring MVC",
+      excerpt:
+        "A Scalable Application built on the top of Spring following Model-View-Controller architecture.",
+      message: "Stay Tuned",
+    },
   ];
 
   return (
@@ -25,11 +31,11 @@ export default function Upcomming({ ParallaxText }) {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
-            className="bg-slate-900 p-6 rounded-lg"
+            className="relative bg-slate-900 p-6 rounded-lg"
           >
             <h3 className="text-2xl font-bold mb-4">{post.title}</h3>
-            <p className="text-slate-400 mb-4">{post.excerpt}</p>
-            <div className="text-slate-300 hover:text-white underline">
+            <p className="text-slate-400 mb-6">{post.excerpt}</p>
+            <div className="absolute bottom-2 text-slate-300 hover:text-white underline">
               {post.message}
             </div>
           </motion.div>
